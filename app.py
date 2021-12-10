9a4bd15151a3e0f0a6503f30eddbee14679cf42a
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2021-12-09 22:00:20 -0300

    App working

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #%% Connect to PSQL
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as sl
+sns.set_style("darkgrid")
+import streamlit as st
 import psycopg2
 
 host = "localhost"
@@ -12,46 +12,88 @@ password = "password"
 dbname = "fishdb"
 
 #%%
-conn = psycopg2.connect(host=host, user=user, password=password, dbname=dbname)
-curr = conn.cursor()
-
-curr.execute(
-	f"SELECT * FROM fishes WHERE name='bonito';"
-)
-f_id = curr.fetchall()[0][0]
-print(f"albacora id: {f_id}")
-
-curr.execute(
-	f"SELECT * FROM prices WHERE f_id={f_id};"
-	# "DELETE FROM fishes *;"
-)
-
-print(curr.fetchall())
-
-conn.commit()
-curr.close()
-conn.close()
-
-
+def get_statistics(name):
+	name = name.lower()
+	
+	conn = psycopg2.connect(host=host, user=user, password=password, dbname=dbname)
+	curr = conn.cursor()
+	
+	curr.execute(
+		f"SELECT * FROM fishes WHERE name='{name}';"
+	)
+	f_id = curr.fetchall()[0][0]
+	
+	curr.execute(
+		f"SELECT * FROM disembarkation_year WHERE f_id={f_id};"
+	)
+	items = curr.fetchall()
+	disemb_years = [item[1] for item in items]
+	disemb_values = [item[2] for item in items]
+	
+	curr.execute(
+		f"SELECT * FROM production WHERE f_id={f_id};"
+	)
+	items = curr.fetchall()
+	production_years = [item[2] for item in items]
+	production_values = [item[4] for item in items]
+	production_state = [item[3] for item in items]
+	production_state = ["Fresco" if string == "fresh" else "Congelado" for string in production_state]
+	
+	curr.execute(
+		f"SELECT * FROM prices WHERE f_id={f_id};"
+	)
+	
+	items = curr.fetchall()
+	prices_years = [item[2] for item in items]
+	prices_values = [item[3] for item in items]
+	
+	conn.commit()
+	curr.close()
+	conn.close()
+	
+	fig, axes = plt.subplots(nrows=3, figsize=(10, 30))
+	# disembarkation_year plot
+	sns.lineplot(disemb_years, disemb_values, ax=axes[0])
+	axes[0].set_title(f"Desembarque de {name[0].upper() + name[1:]} por año", fontsize=15)
+	axes[0].set_ylabel("Toneladas", fontsize=12)
+	axes[0].set_xlabel("Año", fontsize=12)
+	# production plot
+	sns.lineplot(production_years, production_values, hue=production_state, ax=axes[1])
+	axes[1].set_title(f"Producción de {name[0].upper() + name[1:]} por año", fontsize=15)
+	axes[1].set_ylabel("Toneladas", fontsize=12)
+	axes[1].set_xlabel("Año", fontsize=12)
+	# prices plot
+	sns.lineplot(prices_years, prices_values, ax=axes[2])
+	axes[2].set_title(f"Precios de {name[0].upper() + name[1:]} por año", fontsize=15)
+	axes[2].set_ylabel("Precio por tonelada", fontsize=12)
+	axes[2].set_xlabel("Año", fontsize=12)
+	plt.show()
+	
+	return fig, prices_values
+	
 #%%
-
 def main():
 	# Page
 	html_temp="""
-	<h1 style="color:#181082;text-align:center;">Linear Model</h1>
+	<h1 style="color:#ffffff;text-align:center;">Estadísticas de Pesca</h1>
 	</div>
 	"""
 	# Data
-	sl.markdown(html_temp, unsafe_allow_html=True)
-	x1 = sl.text_input("X1")
-	x2 = sl.text_input("X2")
+	st.markdown(html_temp, unsafe_allow_html=True)
+	name = st.text_input("Nombre")
 	# Button
-	if sl.button("Enter:"):
-		x_in = [np.float_(x1.title()),
-				np.float_(x2.title())]
-		predicts = model_prediction(x_in, model)
-		sl.success(f": {predicts}")
-
+	if st.button("Obtener Gráficos:"):
+		try:
+			fig, prices = get_statistics(name)
+			st.pyplot(fig)
+			mean = int(np.mean(prices) + 0.5)
+			mean = f"{mean:,}".replace(",", ".")
+			st.success(f"Media de precios: {mean}")
+			std = int(np.std(prices) + 0.5)
+			std = f"{std:,}".replace(",", ".")
+			st.success(f"Desviación de precios: {std}")
+		except:
+			st.success(f"{name} no se encuentra en la base de datos.")
 
 if __name__ == "__main__":
 	main()
